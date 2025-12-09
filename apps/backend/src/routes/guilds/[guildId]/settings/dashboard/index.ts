a49f65ac221703bc119b44f2777bff4aecb7cd01
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-12-09 13:39:37 -0500

    Created new Guild Settings Routes
    Created endpoints for fetching/updating Logging settings and Dashboard Settings

--- apps/backend/src/routes/guilds/[guildId]/settings/dashboard/index.ts
+++ apps/backend/src/routes/guilds/[guildId]/settings/dashboard/index.ts
@@ -1,9 +1,82 @@
 import express from "express";
+import { DashboardSettings } from "../../../../../models/DashboardSettings";
+import { AppDataSource } from "../../../../..";
+import z, { treeifyError } from "zod";
+import { requireAuth } from "../../../../../lib/requireAuth - Middleware";
+import da from "zod/v4/locales/da.js";
 
 export const router = express.Router({ mergeParams: true });
 
 // Retrieve dashboard settings for a guild
-router.get("/", (req: express.Request<{ guildId: string }>, res) => {});
+router.get(
+	"/",
+	requireAuth,
+	async (req: express.Request<{ guildId: string }>, res) => {
+		const { guildId } = req.params;
 
-// Update dashboard settings for a guild
-router.patch("/", (req: express.Request<{ guildId: string }>, res) => {});
+		const dashRepo = AppDataSource.getRepository(DashboardSettings);
+
+		await dashRepo.upsert(
+			{
+				guildId: guildId,
+			},
+			{ conflictPaths: ["guildId"], skipUpdateIfNoValuesChanged: true }
+		);
+
+		const settings = await dashRepo.findOneBy({ guildId });
+
+		if (!settings) {
+			return res.status(500).send("Failed to retrieve dashboard settings");
+		}
+
+		return res.status(200).json(settings);
+	}
+);
+
+// Update dashboard settings for a
+const patchItems = z.object({
+	rolesWithDashboardViewAccess: z.array(z.string()),
+	rolesWithDashboardEditAccess: z.array(z.string()),
+});
+router.patch(
+	"/",
+	requireAuth,
+	async (req: express.Request<{ guildId: string }>, res) => {
+		const { guildId } = req.params;
+		const parseResult = patchItems.safeParse(req.body);
+
+		if (!parseResult.success) {
+			return res.status(400).json({
+				error: "Invalid request body",
+				details: treeifyError(parseResult.error),
+			});
+		}
+
+		const { rolesWithDashboardViewAccess, rolesWithDashboardEditAccess } =
+			parseResult.data;
+
+		if (!rolesWithDashboardViewAccess && !rolesWithDashboardEditAccess) {
+			return res
+				.status(400)
+				.json({ error: "At least one field must be provided to update." });
+		}
+
+		const dashRepo = AppDataSource.getRepository(DashboardSettings);
+
+		try {
+			await dashRepo.upsert(
+				{
+					guildId: guildId,
+					rolesWithDashboardViewAccess,
+					rolesWithDashboardEditAccess,
+				},
+				{ conflictPaths: ["guildId"], skipUpdateIfNoValuesChanged: true }
+			);
+			return res.status(204).send();
+		} catch (error) {
+			return res
+				.status(500)
+				.json({ error: "Failed to update dashboard settings" });
+		}
+	}
+);
--- apps/backend/src/routes/guilds/[guildId]/settings/logging/index.ts
+++ apps/backend/src/routes/guilds/[guildId]/settings/logging/index.ts
@@ -1,8 +1,100 @@
 import express from "express";
+import { requireAuth } from "../../../../../lib/requireAuth - Middleware";
+import { LoggingSettings } from "../../../../../models/Moderation/Logging/ServerLoggingSettings";
+import { AppDataSource } from "../../../../..";
+import z from "zod";
 export const router = express.Router({ mergeParams: true });
 
 // Retrieve logging settings for a guild
-router.get("/", (req: express.Request<{ guildId: string }>, res) => {});
+router.get(
+	"/",
+	requireAuth,
+	async (req: express.Request<{ guildId: string }>, res) => {
+		const { guildId } = req.params;
 
+		const logSettingsRepo = AppDataSource.getRepository(LoggingSettings);
+
+		await logSettingsRepo.upsert(
+			{
+				guildId: guildId,
+			},
+			{ conflictPaths: ["guildId"], skipUpdateIfNoValuesChanged: true }
+		);
+
+		const settings = await logSettingsRepo.findOneBy({ guildId });
+
+		if (!settings) {
+			return res.status(500).send("Failed to retrieve logging settings");
+		}
+
+		return res.status(200).json(settings);
+	}
+);
 // Update logging settings for a guild
-router.patch("/", (req: express.Request<{ guildId: string }>, res) => {});
+
+const patchItems = z.object({
+	enabled: z.boolean().optional(),
+	defaultLoggingChannelId: z.string(),
+	logModerationActions: z.boolean(),
+	moderationLogChannelId: z.string(),
+	logMessageEditsAndDeletions: z.boolean(),
+	messageLogChannelId: z.string(),
+	logMemberLeaves: z.boolean(),
+	logMemberJoins: z.boolean(),
+	logMemberJoinChannelId: z.string(),
+	logMemberLeaveChannelId: z.string(),
+});
+router.patch(
+	"/",
+	requireAuth,
+	async (req: express.Request<{ guildId: string }>, res) => {
+		const { guildId } = req.params;
+
+		const parseResult = patchItems.safeParse(req.body);
+
+		if (!parseResult.success) {
+			return res
+				.status(400)
+				.json({ error: "Invalid request body", details: parseResult.error });
+		}
+
+		const {
+			enabled,
+			defaultLoggingChannelId,
+			logModerationActions,
+			moderationLogChannelId,
+			logMessageEditsAndDeletions,
+			messageLogChannelId,
+			logMemberJoins,
+			logMemberLeaves,
+			logMemberJoinChannelId,
+			logMemberLeaveChannelId,
+		} = parseResult.data;
+
+		const logSettingsRepo = AppDataSource.getRepository(LoggingSettings);
+
+		try {
+			await logSettingsRepo.upsert(
+				{
+					guildId: guildId,
+					...(enabled !== undefined && { enabled }),
+					defaultLoggingChannelId,
+					logModerationActions,
+					moderationLogChannelId,
+					logMessageEditsAndDeletions,
+					messageLogChannelId,
+					logMemberJoins,
+					logMemberLeaves,
+					logMemberJoinChannelId,
+					logMemberLeaveChannelId,
+				},
+				{ conflictPaths: ["guildId"], skipUpdateIfNoValuesChanged: true }
+			);
+			return res.status(204).send();
+		} catch (error) {
+			return res
+				.status(500)
+				.json({ error: "Failed to update logging settings" });
+		}
+	}
+);
