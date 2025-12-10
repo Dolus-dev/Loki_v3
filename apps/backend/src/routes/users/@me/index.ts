f46a97cbb620aa3bf051aa40b49917e3af7f621c
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-12-09 19:46:19 -0500

    Created endpoint for fetching current user information

--- apps/backend/src/routes/users/@me/index.ts
+++ apps/backend/src/routes/users/@me/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
 import { router as guildsRouter } from "./guilds/index";
+import { requireAuth } from "../../../lib/requireAuth - Middleware";
+import { fetchDiscordUser } from "../../../lib/discordInteractions";
 
 export const router = express.Router();
 router.use("/guilds", guildsRouter);
 
-router.get("/", (req, res) => {
-	res.json({ message: "User info endpoint" });
+router.get("/", requireAuth, async (req, res) => {
+	try {
+		const user = await fetchDiscordUser(req.session.accessToken!);
+
+		return res.status(200).send({
+			id: user.id,
+			username: user.username,
+			avatarHash: user.avatar,
+		});
+	} catch (error) {
+		console.error("Error fetching Discord user in /auth/@me:", error);
+		return res.status(401).json({ error: "Unauthorized" });
+	}
 });
