088fd1b1b588df328f75a461ccf292c97ab3c800
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-12-15 20:25:58 -0500

    Created skeleton route
    Route will be used to create a new row in the guilds table inside the database when the bot joins a new server

--- apps/backend/src/routes/guilds/index.ts
+++ apps/backend/src/routes/guilds/index.ts
@@ -3,3 +3,5 @@ import { router as GuildsRouter } from "./[guildId]/index";
 export const router = express.Router();
 
 router.use("/:guildId", GuildsRouter);
+
+router.post("/", async (req, res) => {});
