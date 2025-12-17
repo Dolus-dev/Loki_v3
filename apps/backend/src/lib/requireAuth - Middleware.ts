640f726dc7dd937d7b600f888b3b026fab197150
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-12-16 19:48:16 -0500

    Updated Authorization Middleware
    Added check for bot access token. Now Bot won't be locked out from accessing the API

--- apps/backend/src/lib/requireAuth - Middleware.ts
+++ apps/backend/src/lib/requireAuth - Middleware.ts
@@ -1,11 +1,29 @@
+import { timingSafeEqual } from "crypto";
 import { Request, Response, NextFunction } from "express";
 
 /**
  *  Middleware to require session authentication for a route
  */
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-	if (!req.session.userId) {
+	const token = req.headers.authorization?.replace("Bot ", "");
+
+	if (!token && !req.session.userId) {
 		console.log("Unauthorized access attempt to protected route");
+		return res.status(401).send({ error: "Unauthorized" });
+	}
+	if (req.session.userId) {
+		return next();
+	}
+
+	const tokenValid = timingSafeEqual(
+		Buffer.from(token as string),
+		Buffer.from(process.env.BOT_API_SECRET as string)
+	);
+
+	if (!tokenValid) {
+		console.log(
+			"Unauthorized access attempt to protected route with invalid token"
+		);
 		return res.status(401).send({ error: "Unauthorized" });
 	}
 	return next();
