2177c88349d9a85a942887cca2e09aeb72aeb916
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-12-16 19:49:26 -0500

    Renamed folder [guildId] => guildId

--- apps/backend/src/routes/guilds/index.ts
+++ apps/backend/src/routes/guilds/index.ts
@@ -1,7 +1,17 @@
 import express from "express";
-import { router as GuildsRouter } from "./[guildId]/index";
+import { router as GuildsRouter } from "./guildId/index";
+import z from "zod";
+import { requireAuth } from "../../lib/requireAuth - Middleware";
 export const router = express.Router();
 
 router.use("/:guildId", GuildsRouter);
 
-router.post("/", async (req, res) => {});
+const newGuildSchema = z.object({
+	id: z.string().min(1),
+	name: z.string().min(1),
+	iconHash: z.string().nullable().optional(),
+});
+router.post("/", requireAuth, async (req, res) => {
+	console.log(`Bot request received.`);
+	return res.status(201).send({ success: true });
+});
