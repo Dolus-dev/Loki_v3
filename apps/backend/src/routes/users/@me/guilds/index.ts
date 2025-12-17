8fe6ef4dc3505fd363d0fcc0c0a331134204d635
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2025-12-17 17:45:01 -0500

    Implemented Guilds Fetch Route

--- apps/backend/src/routes/users/@me/guilds/index.ts
+++ apps/backend/src/routes/users/@me/guilds/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { requireAuth } from "../../../../lib/requireAuth - Middleware";
-import { fetchCurrentUserGuilds } from "../../../../lib/discordInteractions";
+import {
+	fetchCurrentGuildMember,
+	fetchCurrentUserGuilds,
+} from "../../../../lib/discordInteractions";
 import { PermissionsBitField } from "discord.js";
 import { AppDataSource } from "../../../..";
 import { In } from "typeorm";
+import { Guild } from "../../../../models/Guild";
+import { APIGuildMember } from "discord.js";
 
 export const router = express.Router();
 
@@ -17,23 +22,53 @@ router.get("/", requireAuth, async (req, res) => {
 
 	const guilds = await fetchCurrentUserGuilds(user);
 	const userGuildIds = guilds.map((g) => g.id);
-	const guildRepository = AppDataSource.getRepository("Guild");
+	const guildRepository = AppDataSource.getRepository(Guild);
 	const botGuilds = await guildRepository.find({
 		where: { id: In(userGuildIds) },
+		relations: ["dashboardSettings"],
 	});
 
-	console.log("Bot guilds:", botGuilds);
+	const botGuildMap = new Map(botGuilds.map((g) => [g.id, g]));
 
-	console.log("Fetched guilds for user:", guilds);
+	const memberObjects = await Promise.allSettled(
+		botGuilds.map((guild) => fetchCurrentGuildMember(user, guild.id))
+	);
+
+	const guildMemberMap = new Map<string, APIGuildMember | null>(
+		botGuilds.map((g, index) =>
+			memberObjects[index].status === "fulfilled"
+				? [g.id, memberObjects[index].value]
+				: [g.id, null]
+		)
+	);
 
 	const accessibleGuilds = guilds.filter((guild) => {
 		const permissions = new PermissionsBitField(
 			BigInt(guild.permissions as string)
 		);
-		return permissions.has(PermissionsBitField.Flags.ManageGuild);
+
+		if (permissions.has(PermissionsBitField.Flags.ManageGuild)) return true;
+
+		const member = guildMemberMap.get(guild.id);
+		const guildFromBot = botGuildMap.get(guild.id);
+		if (!member || !guildFromBot?.dashboardSettings) return false;
+
+		const viewRoles =
+			guildFromBot?.dashboardSettings?.rolesWithDashboardViewAccess ?? [];
+		const editRoles =
+			guildFromBot?.dashboardSettings?.rolesWithDashboardEditAccess ?? [];
+
+		const allowedRoles = new Set([...viewRoles, ...editRoles]);
+		const hasAccess = member.roles.some((roleId) => allowedRoles.has(roleId));
+		return hasAccess;
 	});
 
-	// console.log("Filtered guilds (Manage Guild access):", accessibleGuilds);
+	const simplifiedGuilds = accessibleGuilds.map((guild) => ({
+		id: guild.id,
+		name: guild.name,
+		icon: guild.icon ?? null,
+		setUp: botGuildMap.has(guild.id),
+	}));
 
-	return res.status(200).send(accessibleGuilds);
+	return res.status(200).send(simplifiedGuilds);
 });
